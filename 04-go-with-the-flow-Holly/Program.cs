9418289b4dc7091662c56196fe29037fe7c0208e
Holly Rhodes <hllyrhodes@gmail.com>
2016-06-16 18:17:42 -0400

    Flow chart without loop

---
+++ 04-go-with-the-flow-Holly/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_go_with_the_flow_Holly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Are you happy?");
+            string answer = Console.ReadLine();
+
+            if (answer.ToLower() == "yes") 
+            {
+                Console.WriteLine("The End.");
+            }
+
+            else 
+            {
+                Console.WriteLine("Get another cat!");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
